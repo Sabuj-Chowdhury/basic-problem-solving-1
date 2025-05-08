24e963a7fd537b2fdc2c1269e2afb79c289a2446
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-05-08 21:09:45 +0530

    problem -2

--- solution.ts
+++ solution.ts
@@ -5,3 +5,17 @@ function formatString(input: string, toUpper?: boolean) {
     return input.toLowerCase();
   }
 }
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+console.log(filterByRating(books));
+// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
