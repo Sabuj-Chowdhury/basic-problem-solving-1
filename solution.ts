adca7dfde8d85859474f5fccabc321771ca03283
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-05-08 21:16:19 +0530

    problem -3

--- solution.ts
+++ solution.ts
@@ -11,11 +11,7 @@ function filterByRating(
 ): { title: string; rating: number }[] {
   return items.filter((item) => item.rating >= 4);
 }
-const books = [
-  { title: "Book A", rating: 4.5 },
-  { title: "Book B", rating: 3.2 },
-  { title: "Book C", rating: 5.0 },
-];
 
-console.log(filterByRating(books));
-// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  return ([] as T[]).concat(...arrays);
+}
